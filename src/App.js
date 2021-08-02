1f90671ca01c85200a8cced0aada4731562da4b0
NemanjaMijic <nemanjamijic@yahoo.com>
2021-08-02 13:15:02 +0200

    App for fetching 10 random recipes based on search query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
+const App = () =>{
+
+  const APP_ID = '9038e56e';
+  const APP_KEY = '6771f69c3f415d8efa06898322ce6fd7';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
+
+
+  useEffect( () => {
+    getRecipes();
+  },[query])
+
+  const getRecipes = async () =>{
+    const response = await fetch(`http://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+        <button type="submit" className="search-button">Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map( recipe => (
+          <Recipe 
+            key={Math.random()*1000} 
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}>
+          </Recipe>
+        ))}
+      </div>
     </div>
   );
 }
